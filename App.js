5f08196997c8e2cb264d7cfbd3979a764d7f784c
ritulanand <ritulanand1997@gmail.com>
2023-12-07 23:12:49 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,17 @@
+// const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "Hello World From React");
+// console.log(heading); // object
+
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child"}, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag")
+    ]),
+    React.createElement("div", {id: "child2"}, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag")
+    ]),
+    ]);
+
+console.log(parent); // object
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
