7e484e7fe892e64f4a95fb567f0c39c627411a98
ritulanand <ritulanand1997@gmail.com>
2023-12-10 14:24:46 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,17 +1,51 @@
 // const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "Hello World From React");
 // console.log(heading); // object
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement("div", {id: "parent"}, [
-    React.createElement("div", {id: "child"}, [
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag")
-    ]),
-    React.createElement("div", {id: "child2"}, [
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag")
-    ]),
-    ]);
+// const parent = React.createElement("div", {id: "parent"}, [
+//     React.createElement("div", {id: "child"}, [
+//         React.createElement("h1", {}, "I'm an h1 tag"),
+//         React.createElement("h2", {}, "I'm an h2 tag")
+//     ]),
+//     React.createElement("div", {id: "child2"}, [
+//         React.createElement("h1", {}, "I'm an h1 tag"),
+//         React.createElement("h2", {}, "I'm an h2 tag")
+//     ]),
+//     ]);
+const Header = () => {
+    return (
+        <div className="header">
+            <div className='logo-container'>
+                <img className='logo' src="https://picsum.photos/id/237/200/300" alt="image" />
 
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                    <li>
+                        About Us
+                    </li>
+                    <li>
+                        Contact Us
+                    </li>
+                    <li>
+                        Cart
+                    </li>
+                </ul>
+                </div>  
+                   </div>
+    )
+}
+const AppLayout = () => {
+    return (
+        <div className='app'>
+            <Header />
+        </div>
+    )
+}
 console.log(parent); // object
 const root= ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<AppLayout />);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React 😀
+
+#Parcel
+-Dev build
+-Local Server
+-HMR=(Hot Module Replacement)
+-file watching algorithm - writte in c++
+-caching - faster builds
+-Image optimization
+-Minification
+-Bundling
+-compress
+-consistent hashing
+-code splitting
+-differential bundling- support for older browser
+-diagnostic
+-error handling
+-https
+-tree shaking - remove unused code
+-different dev and prod bundles
